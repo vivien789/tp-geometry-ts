24a3610cd450812ad73f2a8f3c86fe51094c5e93
vivien789 <vivien.boucher22@gmail.com>
2025-11-15 14:40:38 +0100

    enveloppe builder test supplémentaire

--- test/EnveloppeBuilder.spec.ts
+++ test/EnveloppeBuilder.spec.ts
@@ -7,15 +7,18 @@ describe("test Enveloppe Builder", () => {
         const b = new EnveloppeBuilder([1.0,2.0]);
         {
             b.insert([0.0,1.0]);
-            b.build();
+            const e = b.build();
+            expect(e.toString()).to.equal(`Enveloppe([0,1], [1,2])`);
         }
         {
             b.insert([2.0,0.0]);
-            b.build();
+            const e = b.build();
+            expect(e.toString()).to.equal(`Enveloppe([0,0], [2,2])`);
         }
         {
             b.insert([0.0,3.0]);
-            b.build();
+            const e = b.build();
+            expect(e.toString()).to.equal(`Enveloppe([0,0], [2,3])`);
         }       
         
     });
